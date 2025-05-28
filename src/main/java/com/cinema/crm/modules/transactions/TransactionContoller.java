0a4b3fff139729eacbbc2fb3db3762158c166acc
Sazzad Alom <sazzad.alom@aurusit.com>
2025-05-28 11:27:05 +0530

    Create TransactionContoller.java

---
+++ src/main/java/com/cinema/crm/modules/transactions/TransactionContoller.java
@@ -0,0 +1,14 @@
+package com.cinema.crm.modules.transactions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/manage/")
+public class TransactionContoller {
+
+	public ResponseEntity<String> getTtrans(){
+		return null;
+	}
+}
