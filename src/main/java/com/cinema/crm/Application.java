4434fe5e63b6ffab81cb278bb1404a24773585c6
Sagar Gaikwad <sagar.gaikwad@aurusit.com>
2025-06-19 11:20:16 +0530

    added cors support

--- src/main/java/com/cinema/crm/Application.java
+++ src/main/java/com/cinema/crm/Application.java
@@ -3,6 +3,9 @@ package com.cinema.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -12,6 +15,16 @@ public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+			}
+		};
 	}
 
  public void onStart() {
