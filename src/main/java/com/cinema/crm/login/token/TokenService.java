4d9848209bc52af9eb63059de37d138b8b660421
Sazzad Alom <sazzad.alom@aurusit.com>
2025-07-18 11:06:44 +0530

    ...

--- src/main/java/com/cinema/crm/login/token/TokenService.java
+++ src/main/java/com/cinema/crm/login/token/TokenService.java
@@ -10,6 +10,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -33,7 +34,8 @@ import jakarta.annotation.PostConstruct;
 public class TokenService {
 	
 
-	private int exparyTime = 30;
+	@Value("${expiry-time}")
+	private int exparyTime;
 	private SecretKey secretKey ;
 	@Autowired private RoleRepository roleRepository;
 	
